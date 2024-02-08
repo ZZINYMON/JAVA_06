5bd38095aacc126cc4dceb6559dd261e3d3316bf
Java_Literal <zziny725@icloud.com>
2024-02-08 13:20:40 +0900

    Commit

---
+++ chap11-generics-lecture-source/chap12-collection-lecture-source/src/main/java/com/ohgiraffers/section01/list/run/Application2.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.section01.list.run;
+
+import com.ohgiraffers.section01.list.dto.BookDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application2 {
+
+    /* 수업목표. ArrayList의 정렬 기능을 이용할 수 있다. */
+
+    List<BookDTO> bookList = new ArrayList<>();
+
+    bookList.add(new BookDTO(1, "홍길동전", "허균", 50000));
+
+
+}
